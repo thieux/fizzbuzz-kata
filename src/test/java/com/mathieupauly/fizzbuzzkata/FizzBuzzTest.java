275d4a7ba65d39357cfbdddd9d8b51c76b46f96f
Mathieu Pauly <mathieu.pauly@gmail.com>
2016-06-27 19:49:22 +0200

    Introduce failing test: 1 -> "1"

---
+++ src/test/java/com/mathieupauly/fizzbuzzkata/FizzBuzzTest.java
@@ -0,0 +1,18 @@
+package com.mathieupauly.fizzbuzzkata;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class FizzBuzzTest {
+    @Test
+    public void one() {
+        assertEquals("1", fizzBuzz(1));
+    }
+
+    private String fizzBuzz(int n) {
+        return null;
+    }
+
+
+}
