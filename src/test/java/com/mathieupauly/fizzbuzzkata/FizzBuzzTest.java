79f8885e549a579f3087c119bd0bf47930411fa1
Mathieu Pauly <mathieu.pauly@gmail.com>
2016-06-27 19:52:27 +0200

    Make pass test: 5 -> "Buzz".

--- src/test/java/com/mathieupauly/fizzbuzzkata/FizzBuzzTest.java
+++ src/test/java/com/mathieupauly/fizzbuzzkata/FizzBuzzTest.java
@@ -21,6 +21,9 @@ public class FizzBuzzTest {
     }
 
     private String fizzBuzz(int n) {
+        if (n % 5 == 0) {
+            return "Buzz";
+        }
         if (n % 3 == 0) {
             return "Fizz";
         }
